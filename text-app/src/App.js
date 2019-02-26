985c1c4ed47f3ff70b524a848591fb4501e6afad
Bob Liberatore <bliberat5@usclermg7115.global.publicisgroupe.net>
2019-02-26 15:59:23 -0500

    base form setup

--- text-app/src/App.js
+++ text-app/src/App.js
@@ -3,25 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      history: [{
+
+      }],
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <TextEntryArea />
+        <Undo />
       </div>
     );
+  }
+}
+
+class TextEntryArea extends Component {
+  render(){
+    return(<textarea>hello</textarea>);
+  }
+}
+
+class Undo extends Component {
+  render(){
+    return(<button>Undo</button>);
   }
 }
 
