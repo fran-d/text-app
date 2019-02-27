6cc2a70846d9efb5e38792dce5d63683630812f4
Bob Liberatore <bliberat5@usclermg7115.global.publicisgroupe.net>
2019-02-27 14:44:15 -0500

    proxy additions

--- text-app/src/App.js
+++ text-app/src/App.js
@@ -3,33 +3,54 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      history: [{
+  state = {users: []}
 
-      }],
-    }
+  componentDidMount(){
+    fetch('/users').then(res=>res.json()).then(users=>this.setState({users}));
   }
   render() {
     return (
       <div className="App">
-        <TextEntryArea />
-        <Undo />
+       <TextEntryArea />
+        {this.state.users.map(user =>
+          <div key={user}>{user}</div>
+        )}
       </div>
     );
   }
 }
 
 class TextEntryArea extends Component {
-  render(){
-    return(<textarea>hello</textarea>);
+  constructor(props){
+    super (props);
+    this.state = {value:"hello4"};
+    this.setState.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.myRef = React.createRef();
   }
-}
+  getInitialState(){
+    return this.props.name;
+  }
+  handleChange(event){
+    this.setState({value: event.target.value});
+  }
+  
+  handleClick(event) {
+    console.log(this.getData().value);
+    fetch('/users?dataVal='+this.getData().value, {method: 'post'}).then(res=>res.json()).then(users=>this.setState({users}));
+  }
 
-class Undo extends Component {
+  getData(){
+    return this.state;
+  }
   render(){
-    return(<button>Undo</button>);
+    return(
+      <form>
+              <textarea value={this.state.value} ref={this.myRef} onChange={this.handleChange}/>
+              <input onClick={this.handleClick} type="button" value="Undo"/>
+      </form>
+    );
   }
 }
 
